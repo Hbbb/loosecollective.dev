1935b10c72628e91a00d385b560857289d3a87ce
Harrison <h.borges10592@gmail.com>
2024-09-13 11:09:50 -0600

    add retro arcade styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        arcadeRed: "#FF0000",
+        arcadeYellow: "#FFFF00",
+        arcadePurple: "#800080",
+      },
+      fontFamily: {
+        "press-start": ['"Press Start 2P"', "cursive"],
+      },
+      animation: {
+        blink: "blink 1s step-end infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { visibility: "hidden" },
+          "50%": { visibility: "visible" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
