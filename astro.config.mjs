15e9ba519846c6639e6536ed7d4758a074c8bc89
Harrison <h.borges10592@gmail.com>
2024-11-03 09:25:57 -0700

    add blog, add some deno config
    the deno bits don't work, but I'm hopeful

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()],
+  adapter: deno(),
+  output: "static",
 });
